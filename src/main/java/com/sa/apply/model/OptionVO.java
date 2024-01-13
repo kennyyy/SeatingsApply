0ac6e6e620dfabc88b35ab946211e77bc6ea9d56
kennyyy <kennyyy@naver.com>
2024-01-13 16:02:42 +0900

    rebase2 commit

---
+++ src/main/java/com/sa/apply/model/OptionVO.java
@@ -0,0 +1,79 @@
+package com.sa.apply.model;
+
+import java.sql.Timestamp;
+
+public class OptionVO {
+	private String roomnumber;
+	private String numcount;
+	private Timestamp deadline;
+	private String width;
+	private String height;
+	private String mid;
+	
+	public OptionVO() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public OptionVO(String roomnumber, String numcount, Timestamp deadline, String width, String height, String mid) {
+		super();
+		this.roomnumber = roomnumber;
+		this.numcount = numcount;
+		this.deadline = deadline;
+		this.width = width;
+		this.height = height;
+		this.mid = mid;
+	}
+
+	public String getRoomnumber() {
+		return roomnumber;
+	}
+
+	public void setRoomnumber(String roomnumber) {
+		this.roomnumber = roomnumber;
+	}
+
+	public String getNumcount() {
+		return numcount;
+	}
+
+	public void setNumcount(String numcount) {
+		this.numcount = numcount;
+	}
+
+	public Timestamp getDeadline() {
+		return deadline;
+	}
+
+	public void setDeadline(Timestamp deadline) {
+		this.deadline = deadline;
+	}
+
+	public String getWidth() {
+		return width;
+	}
+
+	public void setWidth(String width) {
+		this.width = width;
+	}
+
+	public String getHeigth() {
+		return height;
+	}
+
+	public void setHeight(String height) {
+		this.height = height;
+	}
+
+	public String getMid() {
+		return mid;
+	}
+
+	public void setMid(String mid) {
+		this.mid = mid;
+	}
+
+	
+	
+	
+	
+}
